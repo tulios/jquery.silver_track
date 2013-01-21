2495b10fba4c509fcdb79580aa0d6c8333809f12
tulios <ornelas.tulio@gmail.com>
2013-01-20 22:00:53 -0200

    updating example

--- js/example1.js
+++ js/example1.js
@@ -5,25 +5,41 @@
 jQuery(function() {
 
   var example = $("#example-1");
+  var parent = example.parents(".track");
   var track = example.silverTrack();
 
   track.install(new SilverTrack.Plugins.Navigator({
-    prev: $("a.prev", example.parent().parent()),
-    next: $("a.next", example.parent().parent())
+    prev: $("a.prev", parent),
+    next: $("a.next", parent)
   }));
 
   track.install(new SilverTrack.Plugins.BulletNavigator({
-    container: $(".bullet-pagination", example.parent().parent())
+    container: $(".bullet-pagination", parent)
+  }));
+
+  track.install(new SilverTrack.Plugins.ResponsiveHubConnector({
+    layouts: ["phone", "small-tablet", "tablet", "web"],
+    onReady: function(track, options, event) {
+      options.onChange(track, options, event);
+    },
+
+    onChange: function(track, options, event) {
+      track.options.mode = "horizontal";
+      track.options.autoheight = false;
+      track.options.perPage = 4;
+
+      if (event.layout === "small-tablet") {
+        track.options.perPage = 3;
+
+      } else if (event.layout === "phone") {
+        track.options.mode = "vertical";
+        track.options.autoHeight = true;
+      }
+
+      track.restart({keepCurrentPage: true});
+    }
   }));
 
   track.start();
-
-  //====
-  // Extra
-  //====
-  $("a.reload", example.parent()).click(function(e) {
-    e.preventDefault();
-    track.restart();
-  });
 
 });
