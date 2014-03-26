c21bf852239668d2f0745a074f69c708c1a39305
tulios <ornelas.tulio@gmail.com>
2014-03-25 22:33:47 -0300

    examples for css3 animation plugin

--- _site/javascripts/plugin_css3_animation_example.js
+++ _site/javascripts/plugin_css3_animation_example.js
@@ -1,6 +1,9 @@
 $(function() {
-  var container = $(".track:first");
-  var track = container.find(".slider-container").silverTrack();
+  // =========================================================================
+  // Without options
+  // =========================================================================
+  var container = $(".track.sample1");
+  var track = container.find(".slider-container").silverTrack({duration: 800});
 
   track.install(new SilverTrack.Plugins.Navigator({
     prev: $("a.prev", container),
@@ -8,6 +11,49 @@ $(function() {
   }));
 
   track.install(new SilverTrack.Plugins.Css3Animation());
+
+  track.start();
+
+  // =========================================================================
+  // Changinh the easing
+  // =========================================================================
+  container = $(".track.sample2");
+  track = container.find(".slider-container").silverTrack({
+    duration: 1000,
+    easing: "easeInOutQuad"
+  });
+
+  track.install(new SilverTrack.Plugins.Navigator({
+    prev: $("a.prev", container),
+    next: $("a.next", container)
+  }));
+
+  track.install(new SilverTrack.Plugins.Css3Animation());
+
+  track.start();
+
+  // =========================================================================
+  // Vertical track with autoHeight and some options
+  // =========================================================================
+  container = $(".track.sample3");
+  track = container.find(".slider-container").silverTrack({
+    duration: 600,
+    easing: "easeInOutQuad",
+    mode: "vertical",
+    autoHeight: true
+  });
+
+  track.install(new SilverTrack.Plugins.Navigator({
+    prev: $("a.prev", container),
+    next: $("a.next", container)
+  }));
+
+  track.install(new SilverTrack.Plugins.Css3Animation({
+    delayUnit: "s",
+    autoHeightDuration: 300,
+    autoHeightEasing: "easeInOutCubic",
+    autoHeightDelay: 1
+  }));
 
   track.start();
 });
